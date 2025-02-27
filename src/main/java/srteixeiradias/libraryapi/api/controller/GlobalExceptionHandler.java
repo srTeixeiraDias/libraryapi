ccaeae4ddd42276cca535ed408ad62761500e528
Samuel Teixeira Dias <samuelteixeiradias008@gmail.com>
2025-02-27 00:21:51 -0300

    fix(exceptions) : fix NotFoundException handlign

--- src/main/java/srteixeiradias/libraryapi/api/controller/GlobalExceptionHandler.java
+++ src/main/java/srteixeiradias/libraryapi/api/controller/GlobalExceptionHandler.java
@@ -1,11 +1,9 @@
 package srteixeiradias.libraryapi.api.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -60,12 +58,20 @@ public class GlobalExceptionHandler {
         }
 
         static ApiError from(IllegalArgumentException ex){
-            return new ApiError("Parametro invalido: "+ ex.getMessage(), null);
+            List<Error> errors = List.of(new Error("Parametro invalido: " + ex.getMessage()));
+            return new ApiError(GENERIC_ERROR_MESSAGE, errors);
+        }
+
+        static ApiError from(NotFoundException ex) {
+            log.error(ex.getMessage(), ex);
+            List<Error> errors = List.of(new Error(ex.getMessage()));
+            return new ApiError(GENERIC_ERROR_MESSAGE, errors);
         }
 
         static ApiError from(RuntimeException ex) {
             log.error(ex.getMessage(), ex);
-            return new ApiError(GENERIC_ERROR_MESSAGE, null);
+            List<Error> errors = List.of(new Error(ex.getMessage()));
+            return new ApiError(GENERIC_ERROR_MESSAGE, errors);
         }
     }
 
