4dbaa41a912f74c4e5c613d5034bdacd873a336b
Samuel Teixeira Dias <samuelteixeiradias008@gmail.com>
2025-03-05 00:28:17 -0300

    fix(usuario) : add usuario response

---
+++ src/main/java/srteixeiradias/libraryapi/domain/response/UsuarioResponse.java
@@ -0,0 +1,18 @@
+package srteixeiradias.libraryapi.domain.response;
+
+import srteixeiradias.libraryapi.domain.model.Usuario;
+
+import java.util.UUID;
+
+public record UsuarioResponse(
+        String login,
+        UUID id
+) {
+
+    public static UsuarioResponse fromEntity(Usuario user){
+        return new UsuarioResponse(
+                user.getLogin(),
+                user.getId()
+        );
+    }
+}
