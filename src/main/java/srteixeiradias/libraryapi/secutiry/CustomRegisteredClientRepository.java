8e6be608d3525b006cc4c00d60e096226869b851
Samuel Teixeira Dias <samuelteixeiradias008@gmail.com>
2025-03-29 22:27:25 -0300

    feat(Security) : add a Custom registered client repository

---
+++ src/main/java/srteixeiradias/libraryapi/secutiry/CustomRegisteredClientRepository.java
@@ -0,0 +1,44 @@
+package srteixeiradias.libraryapi.secutiry;
+
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Component;
+import srteixeiradias.libraryapi.service.ClientService;
+
+@Component
+public class CustomRegisteredClientRepository implements RegisteredClientRepository {
+
+    private final ClientService clientService;
+
+    public CustomRegisteredClientRepository(final ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @Override
+    public void save(RegisteredClient registeredClient) {
+
+    }
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return null;
+    }
+
+    @Override
+    public RegisteredClient findByClientId(final String clientId) {
+        final var client = clientService.findByClientId(clientId).toEntity();
+
+        return RegisteredClient
+                .withId(client.getId().toString())
+                .clientId(client.getClientId())
+                .clientSecret(client.getClientSecret())
+                .redirectUri(client.getRedirectURI())
+                .scope(client.getScope())
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .build();
+    }
+}
